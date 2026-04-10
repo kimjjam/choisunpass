eb1161a7453e701d2e7d166f7d7eef2d1f60d82a
kimjjam <2mp2p@naver.com>
2026-04-10 15:42:20 +0900

    chore: 디버그 로그 제거

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -104,9 +104,7 @@ export default function DashboardPage() {
           fetchRecordsRef.current()
         }
       })
-      .subscribe((status, err) => {
-        console.log('[Realtime] status:', status, err ?? '')
-      })
+      .subscribe()
 
     // 리얼타임이 이벤트를 놓칠 경우를 대비한 5초 폴링 백업
     const pollInterval = setInterval(() => fetchRecordsRef.current(), 5000)
